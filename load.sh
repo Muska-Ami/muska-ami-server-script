e0bb64778c1d1035d175cd2621c71738678daa47
夏沫花火zzz <xmcn@1l1.icu>
2024-02-10 17:15:22 +0800

    update

--- load.sh
+++ load.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl "https://install.1l1.icu/utils/system.sh" -o "/opt/ma_ss/utils/system.sh"
-curl "https://install.1l1.icu/utils/color.sh" -o "/opt/ma_ss/utils/color.sh"
+wget "https://install.1l1.icu/utils/system.sh" -O "/opt/ma_ss/utils/system.sh"
+wget "https://install.1l1.icu/utils/color.sh" -O "/opt/ma_ss/utils/color.sh"
 source /opt/ma_ss/utils/system.sh
 source /opt/ma_ss/utils/color.sh
