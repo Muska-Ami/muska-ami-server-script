b1cab767ae463d75eabf5e4a5647ea490c0b0c79
夏沫花火zzz <xmcn@1l1.icu>
2024-02-10 17:35:44 +0800

    fix

--- linux_setup.sh
+++ linux_setup.sh
@@ -3,46 +3,32 @@
 mkdir -p /opt/ma_ss/
 mkdir -p /opt/ma_ss/utils/
 
-if [[ `whoami` != "root" ]];then
+if [[ $(whoami) != "root" ]]; then
   echo "Please use root user for install!"
   exit 1
 fi
 
 # Install wget
 apt update
-apt install wget
+apt install -y wget
 
 wget "https://install.1l1.icu/load.sh" -O "/opt/ma_ss/load.sh"
 source /opt/ma_ss/load.sh
 
 # 大陆服务器模式
-echo "Use CN install mode?(Y/n)"
-select _SEL_CN_INSTALL_MODE in "Y" "y" "N" "n"
-do
-  if [[ $_SEL_CN_INSTALL_MODE = "Y" || $_SEL_CN_INSTALL_MODE = "y" ]];then
-    CN_INSTALL_MODE=true
-  else
-    CN_INSTALL_MODE=false
-  fi
-  if [[ $_SEL_1PANEL = "N" || $_SEL_1PANEL = "n" ]];then
-    echo "Skip."
-  fi
-  break
-done
+echo "Use CN install mode? (Y/n)"
+read -r cn_install_mode
 
-if [[ `SYSTEM_OS_RELEASE` = "unknown" ]];then
-  echo "Unsupported system, exit."
-  exit 1
-fi
+if [[ $cn_install_mode = "Y" || $cn_install_mode = "y" ]]; then
+  CN_INSTALL_MODE=true
 
-if [$CN_INSTALL_MODE];then
   # 配置清华源
   echo 'Setup APT Tsinghua Tuna mirror...'
-  apt install apt-transport-https ca-certificates
+  apt install -y apt-transport-https ca-certificates
   rm -rf /etc/apt/sources.list
-  if [[ `SYSTEM_OS_RELEASE` = "debian" || `SYSTEM_OS_RELEASE` = "kali" ]];then
+  if [[ $(SYSTEM_OS_RELEASE) = "debian" || $(SYSTEM_OS_RELEASE) = "kali" ]]; then
     wget "https://install.1l1.icu/templates/apt/tsinghua-debian.list" -O "/etc/apt/sources.list"
-  elif [[ `SYSTEM_OS_RELEASE` = "ubuntu" ]];then
+  elif [[ $(SYSTEM_OS_RELEASE) = "ubuntu" ]]; then
     wget "https://install.1l1.icu/templates/apt/tsinghua-ubuntu.list" -O "/etc/apt/sources.list"
   fi
   apt update
@@ -51,7 +37,7 @@ fi
 
 # 安装一些工具
 echo "Install tools and connect to Visual Private Network..."
-apt install wget net-tools sudo selinux-tools python3-pip -y
+apt install -y wget net-tools sudo selinux-utils python3-pip
 pip install hyfetch --break-system-packages
 hyfetch
 curl https://tailscale.com/install.sh | sh
@@ -64,46 +50,40 @@ apt upgrade -y
 echo "Done."
 
 # 安装1Panel
-echo "Install 1Panel?(Y/n)"
-select _SEL_1PANEL in "Y" "y" "N" "n"
-do
-  if [[ $_SEL_1PANEL = "Y" || $_SEL_1PANEL = "y" ]];then
-    echo "Installing 1Panel..."
-    curl -sSL https://resource.fit2cloud.com/1panel/package/quick_start.sh -o /opt/ma_ss/temps/quick_start.sh
-    cd /opt/ma_ss/temps/
-    bash /opt/ma_ss/temps/quick_start.sh
-    cd ~/
-    # 清理1Panel安装文件
-    echo "Installed. Cleanup 1Panel cache..."
-    rm -rf /opt/ma_ss/temps/quick_start.sh
-    rm -rf /opt/ma_ss/temps/1panel*
-    echo "Cleanup success!"
-    echo "Setup 1Panel rules"
-    #设置1Panel规则
-    1pctl reset entrance
-    echo "Done."
-  fi
-  if [[ $_SEL_1PANEL = "N" || $_SEL_1PANEL = "n" ]]; then
-    echo "Skip."
-  fi
-  break
-done
+echo "Install 1Panel? (Y/n)"
+read -r install_1panel
+
+if [[ $install_1panel = "Y" || $install_1panel = "y" ]]; then
+  echo "Installing 1Panel..."
+  curl -sSL https://resource.fit2cloud.com/1panel/package/quick_start.sh -o /opt/ma_ss/temps/quick_start.sh
+  cd /opt/ma_ss/temps/ || exit
+  bash /opt/ma_ss/temps/quick_start.sh
+  cd ~/
+  # 清理1Panel安装文件
+  echo "Installed. Cleanup 1Panel cache..."
+  rm -rf /opt/ma_ss/temps/quick_start.sh
+  rm -rf /opt/ma_ss/temps/1panel*
+  echo "Cleanup success!"
+  echo "Setup 1Panel rules"
+  # 设置1Panel规则
+  1pctl reset entrance
+  echo "Done."
+else
+  echo "Skip 1Panel installation."
+fi
 
 # 安装哪吒监控
-echo "Install nezha-agent for Nezha monitor?(Y/n)"
-select _SEL_NEZHA_AGENT in "Y" "y" "N" "n"
-do
-  if [[ $_SEL_NEZHA_AGENT = "Y" || $_SEL_NEZHA_AGENT = "y" ]]; then
-    echo "Installing nezha-agent..."
-    curl -L https://proxy-gh.1l1.icu/raw.githubusercontent.com/naiba/nezha/master/script/install.sh -o ~/nezha.sh
-    sed -i 's/GITHUB_URL="github.com"/GITHUB_URL="proxy-gh.1l1.icu\/github.com"/g' ~/nezha.sh
-    sed -i 's/GITHUB_RAW_URL="raw.githubusercontent.com/GITHUB_RAW_URL="proxy-gh.1l1.icu\/raw.githubusercontent.com/g' ~/nezha.sh
-    chmod +x ~/nezha.sh
-    bash ~/nezha.sh install_agent
-    echo "Done."
-  fi
-  if [[ $_SEL_NEZHA_AGENT = "N" || $_SEL_NEZHA_AGENT = "n" ]]; then
-    echo "Skip."
-  fi
-  break
-done
+echo "Install nezha-agent for Nezha monitor? (Y/n)"
+read -r install_nezha_agent
+
+if [[ $install_nezha_agent = "Y" || $install_nezha_agent = "y" ]]; then
+  echo "Installing nezha-agent..."
+  curl -L https://proxy-gh.1l1.icu/raw.githubusercontent.com/naiba/nezha/master/script/install.sh -o ~/nezha.sh
+  sed -i 's/GITHUB_URL="github.com"/GITHUB_URL="proxy-gh.1l1.icu\/github.com"/g' ~/nezha.sh
+  sed -i 's/GITHUB_RAW_URL="raw.githubusercontent.com/GITHUB_RAW_URL="proxy-gh.1l1.icu\/raw.githubusercontent.com/g' ~/nezha.sh
+  chmod +x ~/nezha.sh
+  bash ~/nezha.sh install_agent
+  echo "Done."
+else
+  echo "Skip nezha-agent installation."
+fi
