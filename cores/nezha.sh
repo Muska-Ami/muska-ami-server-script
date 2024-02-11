4886c1d13219c8cab87edb756c78269c63e0d312
夏沫花火zzz <xmcn@1l1.icu>
2024-02-11 15:18:08 +0800

    update

--- cores/nezha.sh
+++ cores/nezha.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-echo -e "${CLR_BLUE}Installing nezha-agent...${CLR_NC}"
+echo -e "${CLR_BLUE}Installing nezha-agent...${CLR_GRAY}"
 curl -L https://proxy-gh.1l1.icu/raw.githubusercontent.com/naiba/nezha/master/script/install.sh -o ~/nezha.sh
 sed -i 's/GITHUB_URL="github.com"/GITHUB_URL="proxy-gh.1l1.icu\/github.com"/g' ~/nezha.sh
 sed -i 's/GITHUB_RAW_URL="raw.githubusercontent.com/GITHUB_RAW_URL="proxy-gh.1l1.icu\/raw.githubusercontent.com/g' ~/nezha.sh
 chmod +x ~/nezha.sh
+echo -e "${CLR_WHITE}"
 bash ~/nezha.sh install_agent  
-echo -e "${CLR_GREEN}Done.${CLR_NC}"
+echo -e "${CLR_GREEN}Done.${CLR_GRAY}"
--- cores/tailscale.sh
+++ cores/tailscale.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-echo -e "${CLR_BLUE}Install and setup tailscale..."
+echo -e "${CLR_BLUE}Install and setup tailscale...${CLR_GRAY}"
 curl https://tailscale.com/install.sh | sh
+echo -e "${CLR_WHITE}"
 tailscale up
-echo -e "${CLR_GREEN}Done.${CLR_NC}"
+echo -e "${CLR_GREEN}Done.${CLR_GRAY}"
