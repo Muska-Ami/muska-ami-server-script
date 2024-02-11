09f488090aec372ec2ff630816fc42ad6a59e86a
夏沫花火zzz <xmcn@1l1.icu>
2024-02-11 15:11:34 +0800

    update

--- linux_setup.sh
+++ linux_setup.sh
@@ -2,6 +2,7 @@
 
 mkdir -p /opt/ma_ss/
 mkdir -p /opt/ma_ss/utils/
+mkdir -p /opt/ma_ss/cores/
 
 if [[ $(whoami) != "root" ]]; then
   echo -e "${COLOR_RED}Please use root user for install!${COLOR_NC}"
@@ -12,106 +13,84 @@ fi
 apt update
 apt install -y wget
 
-wget "https://install.1l1.icu/load.sh" -O "/opt/ma_ss/load.sh"
+wget "https://install.1l1.icu/load.sh" -O "/opt/ma_ss/load.sh" -q
 source /opt/ma_ss/load.sh
 
-function cn_install() {
-  # 配置清华源
-  if [[ $cn_install_mode = "Y" || $cn_install_mode = "y" ]]; then
-    wget "https://install.1l1.icu/cores/tsinghua-tuna-mirror.sh" -O "/opt/ma_ss/cores/tsinghua-tuna-mirror.sh"
-    source /opt/ma_ss/cores/tsinghua-tuna-mirror.sh
-  elif [[ $cn_install_mode = "N" || $cn_install_mode = "n" ]]; then
-    echo -e "${CLR_YELLOW}Skip CN install step.${CLR_NC}"
-  else
-    echo -e "${CLR_RED}Expected Y/n!${CLR_NC}"
-  fi
-}
-
-function install_tools() {
-  # 安装工具
-  echo -e "${CLR_BLUE}Installing tools...${CLR_NC}"
-  apt install -y wget net-tools sudo selinux-utils python3-pip vim
-  pip install hyfetch --break-system-packages
-  hyfetch &
-  wait
-}
-
-function sync_ntp() {
-  # NTP对时
-  echo -e "${CLR_BLUE}Sync network time...${CLR_NC}"
-  wget "https://install.1l1.icu/cores/ntp-sync.sh" -O "/opt/ma_ss/cores/ntp-sync.sh"
-  source /opt/ma_ss/cores/ntp-sync.sh
-  echo -e "${CLR_GREEN}Done.${CLR_NC}"
-}
-
-function upgrade_packages() {
-  # 更新系统默认包
-  echo -e "${CLR_BLUE}Upgrading system packages...${CLR_NC}"
-  apt upgrade -y
-  echo -e "${CLR_GREEN}Done.${CLR_NC}"
-}
-
-function connect_tailscale() {
-  # Tailscale
-  echo -e "${CLR_CYAN}Connect to Tailscale Visual Private Network? (Y/n)${CLR_NC}"
-  read -p ">" connect_tailscale_vpn
-
-  if [[ $connect_tailscale_vpn = "Y" || $connect_tailscale_vpn = "y" ]]; then
-    wget "https://install.1l1.icu/cores/tailscale.sh" -O "/opt/ma_ss/cores/tailscale.sh"
-    source "/opt/ma_ss/cores/tailscale.sh"
-  elif [[ $connect_tailscale_vpn = "N" || $connect_tailscale_vpn = "n" ]]; then
-    echo -e "${CLR_YELLOW}Skip Tailscale installation.${CLR_NC}"
-  else
-    echo -e "${CLR_RED}Expected Y/n!${CLR_NC}"
-  fi
-}
-
-function install_1panel() {
-  # 安装1Panel
-  echo -e "${CLR_CYAN}Install 1Panel? (Y/n)${CLR_NC}"
-  read -p ">" install_1panel
-
-  if [[ $install_1panel = "Y" || $install_1panel = "y" ]]; then
-    wget "https://install.1l1.icu/cores/1panel.sh" -O "/opt/ma_ss/cores/1panel.sh"
-    source "/opt/ma_ss/cores/1panel.sh"
-  elif [[ $install_1panel = "N" || $install_1panel = "n" ]]; then
-    echo -e "${CLR_YELLOW}Skip 1Panel installation.${CLR_NC}"
-  else
-    echo -e "${CLR_RED}Expected Y/n!${CLR_NC}"
-  fi
-}
-
-function install_nezha_agent() {
-  # 安装哪吒监控
-  echo -e "${CLR_CYAN}Install nezha-agent for Nezha monitor? (Y/n)${CLR_NC}"
-  read -p ">" install_nezha_agent
-
-  if [[ $install_nezha_agent = "Y" || $install_nezha_agent = "y" ]]; then
-    wget "https://install.1l1.icu/cores/nezha.sh" -O "/opt/ma_ss/cores/nezha.sh"
-    source "/opt/ma_ss/cores/nezha.sh"
-  elif [[ $install_nezha_agent = "N" || $install_nezha_agent = "n" ]]; then
-    echo -e "${CLR_YELLOW}Skip nezha-agent installation.${CLR_NC}"
-  else
-    echo -e "${CLR_RED}Expected Y/n!${CLR_NC}"
-  fi
-}
-
-function cleanup_cache() {
-  # 清理缓存
-  echo -e "${CLR_PURPLE}Cleanup cache...${CLR_NC}"
-  rm -rf /opt/ma_ss/
-  echo -e "${CLR_GREEN}Done.${CLR_NC}"
-}
-
-# 大陆服务器模式
+# 配置清华源
 echo -e "${CLR_CYAN}Use CN install mode? (Y/n)${CLR_NC}"
 read -p ">" cn_install_mode
 
-cn_install
-install_tools
-sync_ntp
-upgrade_packages
-connect_tailscale
-install_1panel
-install_nezha_agent
-cleanup_cache
+if [[ $cn_install_mode = "Y" || $cn_install_mode = "y" ]]; then
+  wget "https://install.1l1.icu/cores/tsinghua-tuna-mirror.sh" -O "/opt/ma_ss/cores/tsinghua-tuna-mirror.sh" -q
+  source /opt/ma_ss/cores/tsinghua-tuna-mirror.sh
+elif [[ $cn_install_mode = "N" || $cn_install_mode = "n" ]]; then
+  echo -e "${CLR_YELLOW}Skip CN install step.${CLR_NC}"
+else
+  echo -e "${CLR_RED}Expected Y/n！${CLR_NC}"
+  exit 1
+fi
+
+# 安装工具
+echo -e "${CLR_BLUE}Installing tools...${CLR_NC}"
+apt install -y wget net-tools sudo selinux-utils python3-pip vim
+pip install hyfetch --break-system-packages
+hyfetch &
+wait
+
+# NTP对时
+echo -e "${CLR_BLUE}Sync network time...${CLR_NC}"
+wget "https://install.1l1.icu/cores/ntp-sync.sh" -O "/opt/ma_ss/cores/ntp-sync.sh" -q
+source /opt/ma_ss/cores/ntp-sync.sh
+echo -e "${CLR_GREEN}Done.${CLR_NC}"
+
+# 更新系统默认包
+echo -e "${CLR_BLUE}Upgrading system packages...${CLR_NC}"
+apt upgrade -y
+echo -e "${CLR_GREEN}Done.${CLR_NC}"
+
+# Tailscale
+echo -e "${CLR_CYAN}Connect to Tailscale Visual Private Network? (Y/n)${CLR_NC}"
+read -p ">" connect_tailscale_vpn
+
+if [[ $connect_tailscale_vpn = "Y" || $connect_tailscale_vpn = "y" ]]; then
+  wget "https://install.1l1.icu/cores/tailscale.sh" -O "/opt/ma_ss/cores/tailscale.sh" -q
+  source "/opt/ma_ss/cores/tailscale.sh"
+elif [[ $connect_tailscale_vpn = "N" || $connect_tailscale_vpn = "n" ]]; then
+  echo -e "${CLR_YELLOW}Skip Tailscale installation.${CLR_NC}"
+else
+  echo -e "${CLR_RED}Expected Y/n！${CLR_NC}"
+  exit 1
+fi
+
+# 安装1Panel
+echo -e "${CLR_CYAN}Install 1Panel? (Y/n)${CLR_NC}"
+read -p ">" install_1panel
+
+if [[ $install_1panel = "Y" || $install_1panel = "y" ]]; then
+  wget "https://install.1l1.icu/cores/1panel.sh" -O "/opt/ma_ss/cores/1panel.sh" -q
+  source "/opt/ma_ss/cores/1panel.sh"
+elif [[ $install_1panel = "N" || $install_1panel = "n" ]]; then
+  echo -e "${CLR_YELLOW}Skip 1Panel installation.${CLR_NC}"
+else
+  echo -e "${CLR_RED}Expected Y/n！${CLR_NC}"
+  exit 1
+fi
+
+# 安装哪吒监控
+echo -e "${CLR_CYAN}Install nezha-agent for Nezha monitor? (Y/n)${CLR_NC}"
+read -p ">" install_nezha_agent
+
+if [[ $install_nezha_agent = "Y" || $install_nezha_agent = "y" ]]; then
+  wget "https://install.1l1.icu/cores/nezha.sh" -O "/opt/ma_ss/cores/nezha.sh" -q
+  source "/opt/ma_ss/cores/nezha.sh"
+elif [[ $install_nezha_agent = "N" || $install_nezha_agent = "n" ]]; then
+  echo -e "${CLR_YELLOW}Skip nezha-agent installation.${CLR_NC}"
+else
+  echo -e "${CLR_RED}Expected Y/n！${CLR_NC}"
+  exit 1
+fi
+
+# 清理缓存
+echo -e "${CLR_PURPLE}Cleanup cache...${CLR_NC}"
+rm -rf /opt/ma_ss/
+echo -e "${CLR_GREEN}Done.${CLR_NC}"
--- load.sh
+++ load.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget "https://install.1l1.icu/utils/system.sh" -O "/opt/ma_ss/utils/system.sh"
-wget "https://install.1l1.icu/utils/color.sh" -O "/opt/ma_ss/utils/color.sh"
+wget "https://install.1l1.icu/utils/system.sh" -O "/opt/ma_ss/utils/system.sh" -q
+wget "https://install.1l1.icu/utils/color.sh" -O "/opt/ma_ss/utils/color.sh" -q
 source /opt/ma_ss/utils/system.sh
 source /opt/ma_ss/utils/color.sh
